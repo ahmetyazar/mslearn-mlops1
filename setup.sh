cbe821aa7aa9941f50f9a28430b8a60205493cd4
ahmetyazar <18087599+ahmetyazar@users.noreply.github.com>
2024-04-19 12:59:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 az group create --name rg-dp100-mlops --location eastus
 az ml workspace create --name wks-dp100-mlops --location eastus --resource-group rg-dp100-mlops
 az ml compute create --name yazar --type computeinstance --size Standard_DS1_v2 --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
+az ml data create --name diabetes-dev --version 1 --path https://github.com/ahmetyazar/mslearn-mlops1/tree/main/experimentation/data/diabetes-dev.csv --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
