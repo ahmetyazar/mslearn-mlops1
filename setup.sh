156e58da04b54bcfd7d5b34d9bcf99c7210961af
ahmetyazar <18087599+ahmetyazar@users.noreply.github.com>
2024-04-20 05:59:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,42 @@
-az group create --name rg-dp100-mlops --location eastus
-az ml workspace create --name wks-dp100-mlops --location eastus --resource-group rg-dp100-mlops
-az ml compute create --name yazar --type computeinstance --size Standard_DS1_v2 --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
-az ml data create --name diabetes-dev --version 1 --path https://github.com/ahmetyazar/mslearn-mlops1/tree/main/experimentation/data/diabetes-dev.csv --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
-az ml data create --name diabetes-dev-folder --version 1 --path https://github.com/ahmetyazar/mslearn-mlops1/tree/main/experimentation/data/ --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
+#az ml data create --name diabetes-dev --version 1 --path https://github.com/ahmetyazar/mslearn-mlops1/tree/main/experimentation/data/diabetes-dev.csv --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
+#az ml data create --name diabetes-dev-folder --version 1 --path https://github.com/ahmetyazar/mslearn-mlops1/tree/main/experimentation/data/ --resource-group rg-dp100-mlops --workspace-name wks-dp100-mlops
 
-az ml job create --file ./mslearn-mlops1/src/job.yml --workspace-name wks-dp100-mlops --resource-group rg-dp100-mlops
+#az ml job create --file ./mslearn-mlops1/src/job.yml --workspace-name wks-dp100-mlops --resource-group rg-dp100-mlops
+
+#! /usr/bin/sh
+
+# Create random string
+guid=$(cat /proc/sys/kernel/random/uuid)
+suffix=${guid//[-]/}
+suffix=${suffix:0:18}
+
+# Set the necessary variables
+RESOURCE_GROUP="rg-dp100-l${suffix}"
+RESOURCE_PROVIDER="Microsoft.MachineLearning"
+REGIONS=("eastus" "westus")
+RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+WORKSPACE_NAME="mlw-dp100-l${suffix}"
+COMPUTE_INSTANCE="ci${suffix}"
+COMPUTE_CLUSTER="aml-cluster"
+
+# Register the Azure Machine Learning resource provider in the subscription
+echo "Register the Machine Learning resource provider:"
+az provider register --namespace $RESOURCE_PROVIDER
+
+# Create the resource group and workspace and set to default
+echo "Create a resource group and set as default:"
+az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az configure --defaults group=$RESOURCE_GROUP
+
+echo "Create an Azure Machine Learning workspace:"
+az ml workspace create --name $WORKSPACE_NAME 
+az configure --defaults workspace=$WORKSPACE_NAME 
+
+# Create compute instance
+echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
+#az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS1_V2 --type ComputeInstance
+
+# Create compute cluster
+# echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
+# az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
