9c1410f04df314da42dfa9b44ab13950d1d9f333
ahmetyazar <18087599+ahmetyazar@users.noreply.github.com>
2024-04-28 07:30:37 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -38,7 +38,8 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 
+               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     print(np.unique(y, return_counts=True))
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
@@ -63,6 +64,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
